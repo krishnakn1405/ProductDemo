1e0f637d8c5db3572fbd514f699510583907d047
krishna <krishnakn140@gmail.com>
2026-01-31 16:03:31 +0530

    Added add, fetch and fetch by text functions

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,44 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
         ProductService service = new ProductService();
 
         // hey service add products
-        Product p = new Product("Asus Vivobook", "Laptop", "Brown Table", 2022);
-        service.addProduct(p);
+        service.addProduct(new Product("Type C", "Cable", "Black Drawer", 2024));
+        service.addProduct(new Product("Mac Studio", "Computer", "White Table", 2025));
+        service.addProduct(new Product("Focusrite Mixer", "Audio System", "White Table", 2024));
+        service.addProduct(new Product("Asus Vivobook", "Laptop", "Brown Drawer", 2023));
+        service.addProduct(new Product("Asus ROG", "Laptop", "Black Table", 2021));
+        service.addProduct(new Product("Macbook Pro", "Laptop", "Brown Drawer", 2020));
+        service.addProduct(new Product("Wacom Pad", "Writing Pad", "Black Drawer", 2024));
+        service.addProduct(new Product("Apple Keyboard", "Keyboard", "White Table", 2023));
+        service.addProduct(new Product("Logitech Keyboard", "Keyboard", "Black Table", 2023));
+        service.addProduct(new Product("HDMI Cable", "Cable", "Black Drawer", 2024));
+        service.addProduct(new Product("Java Black Book", "Cable", "Shelf", 2024));
+        service.addProduct(new Product("Logi Mouse", "Mouse", "Black Table", 2022));
+        service.addProduct(new Product("Apple Mouse", "Mouse", "White Table", 2022));
+        service.addProduct(new Product("Lenovo Mouse", "Mouse", "Black Drawer", 2023));
+        service.addProduct(new Product("BlackBeast", "Computer", "White Table", 2026));
+
+//        List<Product> products = service.getAllProducts();
+//        for(Product p: products) {
+//            System.out.println(p);
+//        }
+
+//        System.out.println("===================================");
+//        System.out.println("a Particular Product");
+//
+//        Product p = service.getProduct("Logi Mouse");
+//        System.out.println(p);
+
+        System.out.println("===================================");
+        System.out.println("a Particular text");
+
+        List<Product> prods = service.getProductWithText("black");;
+        for(Product product: prods) {
+            System.out.println(product);
+        }
     }
 }
